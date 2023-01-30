aa2dc0959ca3278932e310740816301d96c60a7d
Stacyde <degtich@mail.ru>
2023-01-30 23:52:00 +0400

    add height and weight

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public float calculate(float a, float b) {
-        float bmi = a / (b * b);
+    public float calculate(float weight, float height) {
+        float bmi = weight / (height * height);
         return bmi;
 
     }
